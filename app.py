b58896585be5f691b2badebe6c64779b8dc1352a
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-03-10 21:28:58 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import boto3
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def load_results_to_s3():
+	client = boto3.client(
+	    's3', 
+	    endpoint_url='https://s3.amazonaws.com',
+	    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+	    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+	)
+	json_data = json.dumps({'test': 'hello'})
+	client.put_object(
+		Bucket=os.getenv('BUCKET_NAME'), 
+		Key=f'real_estate/test.json',
+		Body=json_data
+	)
+
+def main():
+	load_results_to_s3()
+
+
+if __name__ == '__main__':
+	main()
